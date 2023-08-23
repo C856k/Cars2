fad6c5a5c8384494f61823e57c9d1aa4bf86e732
C856k <Carl856k@stud.kea.dk>
2023-08-23 09:44:46 +0200

    First commit

---
+++ src/main/java/dat3/CarsRusApplication.java
@@ -0,0 +1,13 @@
+package dat3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsRusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsRusApplication.class, args);
+    }
+
+}
